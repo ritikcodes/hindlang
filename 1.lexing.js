72930fdabbde670f0e7b252edd14a5d9404c8f59
Ritik Chauhan <mailcritik@gmail.com>
2024-09-06 20:20:12 +0530

    Lexing done

---
+++ 1.lexing.js
@@ -0,0 +1,41 @@
+const { stl } = require("./constants");
+
+// 1. Lexing: Tokenize the code input
+function getTokenProperties(token) {
+  if (Number(token)) {
+    return { type: "Number", value: Number(token) };
+  }
+  if (token === ")" || token === "(") {
+    return { type: "Paranthesis", value: token };
+  }
+
+  if (stl[token]) {
+    return { type: "Function", value: token };
+  }
+
+  if (token === "barabar") {
+    return { type: "VariableDeclaration", value: token };
+  }
+
+  return { type: "Identifier", value: token };
+}
+
+function tokenize(code) {
+  const tokens = [];
+  let current = "";
+
+  for (let i = 0; i < code.length + 1; i++) {
+    if (code[i] === " " || !code[i]) {
+      tokens.push(getTokenProperties(current));
+      current = "";
+    } else {
+      current = current + code[i];
+    }
+  }
+
+  return tokens;
+}
+
+module.exports = {
+  tokenize,
+};
---
+++ constants.js
@@ -0,0 +1,11 @@
+// Standard library
+const stl = {
+  jama: (a, b) => a + b,
+  guna: (a, b) => a * b,
+  bhag: (a, b) => a / b,
+  ghaat: (a, b) => a - b,
+};
+
+module.exports = {
+  stl,
+};
