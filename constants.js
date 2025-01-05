fca79fca219071b457bfca9f51a1090b63a5496b
abhinandanpabdit01 <abhinandanpandit029@gmail.com>
2025-01-05 10:23:29 +0530

    String and new stl is added

--- constants.js
+++ constants.js
@@ -6,7 +6,16 @@ const stl = {
   jama: (a, b) => a + b,
   guna: (a, b) => a * b,
   bhag: (a, b) => a / b,
-  ghaat: (a, b) => a - b,
+  ghaata: (a, b) => a - b,
+  "bada-hai": (a, b) => a > b,
+  "chota-hai": (a, b) => a < b,
+  "bada-barabar-hai": (a, b) => a >= b,
+  "chota-barabar-hai": (a, b) => a <= b,
+  ghat: (a, b) => Math.pow(a, b),
+  shabdjama: (variable, string) => {
+    env[variable] = string;
+    return env[variable];
+  },
 };
 
 module.exports = {
